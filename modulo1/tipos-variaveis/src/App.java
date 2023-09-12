808ed87e8a890a7c65194e385cd651c481968bb9
Guilherme <guibah78@gmail.com>
2023-09-12 13:12:56 -0300

    tipos, variaveis e constantes

--- modulo1/tipos-variaveis/src/App.java
+++ modulo1/tipos-variaveis/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNomal = numeroCurto;
+        short numeroCurto2 = (short) numeroNomal;
+
+        final String CPF = "99900099950"; //constante
+        CPF = "trolei";
     }
 }
