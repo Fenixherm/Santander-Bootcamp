9953fcb649487ae35ccd7c6383ca1ff7cd8ec7cd
Guilherme <guibah78@gmail.com>
2023-09-13 13:03:37 -0300

    Operadores lógicos e outros tipos

--- modulo1/tipos-variaveis/src/Operadores.java
+++ modulo1/tipos-variaveis/src/Operadores.java
@@ -23,8 +23,9 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
         */
         //Operadores Unários
-        int numero = 5;
+        //int numero = 5;
 
+        /*
         numero = - numero; 
 
         System.out.println(numero);
@@ -32,5 +33,33 @@ public class Operadores {
         numero = - numero;
 
         System.out.println(numero);
+        */
+
+        //Operadores de incremento
+        /*
+        int numero = 5;
+        numero++;
+        System.out.println(numero); // 6
+
+        System.out.println(numero++); // 6 =>  aqui o incremento só acontece após a impressão do número
+        System.out.println(numero); // 7
+
+        System.out.println(++ numero); //8 => Aqui o incremento acontece antes de imprimir
+        */
+
+        //Operadores relacionais
+        /*
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 != numero2; // ao realizar uma comparacao e atribuir a um boolean, ele atribui se é true ou false apenas
+
+        System.out.println("Numero "+ numero1 + " é diferente a Numero " + numero2 + " ? " + simNao);
+        */
+        
+        String nomeUm = "Jow";
+        String nomeDois = new String("Jow");
+
+        System.out.println(nomeUm.equals(nomeDois)); // true
     }
 }
