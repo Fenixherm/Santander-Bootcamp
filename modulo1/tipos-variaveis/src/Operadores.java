07d12f8fdb33bbf1f1847d7b8f3e4a5718e0d6fc
Guilherme Augusto <guibah78@gmail.com>
2023-09-12 21:43:49 -0300

    Um pouco sobre operadores

---
+++ modulo1/tipos-variaveis/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+    public static void main(String[] args){
+        //Exemplo de concatenação 
+        /* 
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        */
+        //Operadores Unários
+        int numero = 5;
+
+        numero = - numero; 
+
+        System.out.println(numero);
+
+        numero = - numero;
+
+        System.out.println(numero);
+    }
+}
