bbccbbf2336bd7d310c22e56a7cd19961eb303f8
Guilherme <guibah78@gmail.com>
2023-09-29 14:20:19 -0300

    Modificado a classe computador!

--- modulo3/MSN/src/Computador.java
+++ modulo3/MSN/src/Computador.java
@@ -1,20 +1,24 @@
 import servicos.apps.MSNMessenger;
 import servicos.apps.Telegram;
+import servicos.ServicoMensagemInstantanea;
 import servicos.apps.FacebookMessenger;
 
 public class Computador {
 	public static void main(String[] args) {
-		MSNMessenger msn = new MSNMessenger();		
-		FacebookMessenger fcb = new FacebookMessenger();
-		Telegram tg = new Telegram();
 		
-		msn.enviarMensagem("Hello World!");
-		msn.receberMensagem("Hi!");
+		ServicoMensagemInstantanea smi = null;
 		
-		fcb.enviarMensagem("Hello World!");
-		fcb.receberMensagem("Hi!");
+		String appEscolhido = "msn";
 		
-		tg.enviarMensagem("Hello World!");
-		tg.receberMensagem("Hi!");
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fcb"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("Tg"))
+			smi = new Telegram();
+		
+		
+		smi.enviarMensagem("Bom dia!");
+		smi.receberMensagem("boa noite");
 	}
 }
