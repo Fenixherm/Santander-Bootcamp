d1622ec4f2ae65680ca901a235be94ee422053dd
Guilherme <guibah78@gmail.com>
2023-10-17 17:42:35 -0300

    Classe assertions e alguns testes basicos

--- modulo5/JUnit-Maven/src/test/java/one/junit/AssertionsTeste.java
+++ modulo5/JUnit-Maven/src/test/java/one/junit/AssertionsTeste.java
@@ -1,5 +1,33 @@
 package one.junit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class AssertionsTeste {
+
+    @Test
+    void validaArray(){
+        int [] vetorInt = {10, 15, 20, 25, 30};
+        int [] vetor2 = {10, 15, 20, 25, 30};
+
+        assertArrayEquals(vetorInt, vetor2);
+    }
+
+    @Test
+    void validaNulo(){
+        Pessoa pessoa = null;
+
+        assertNull(pessoa);
+    }
+
+    @Test
+    void validaNotNulo(){
+        Pessoa pessoa = new Pessoa(15);
+
+        assertNotNull(pessoa);
+    }
 
 }
