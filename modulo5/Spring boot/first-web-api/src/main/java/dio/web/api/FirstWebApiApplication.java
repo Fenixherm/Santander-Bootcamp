96e2bcadab278ede4d2d25b70b01634fb0f83597
Guilherme Augusto <guibah78@gmail.com>
2023-10-20 21:00:13 -0300

    Swagger Funcionando, Handling Exception pronto

--- modulo5/Spring boot/first-web-api/src/main/java/dio/web/api/FirstWebApiApplication.java
+++ modulo5/Spring boot/first-web-api/src/main/java/dio/web/api/FirstWebApiApplication.java
@@ -1,12 +1,10 @@
 package dio.web.api;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "First web API", version = "0.1", description = "Web API"))
 public class FirstWebApiApplication {
 
 	public static void main(String[] args) {
